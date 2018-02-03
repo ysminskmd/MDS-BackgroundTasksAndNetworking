4f1836f461ea844f64455f66ce7276a622958eb9
Alexander Klintsov <alexklints@yandex-team.ru>
2018-02-03 23:32:26 +0300

    Emulate deadlock

--- SimpleExample/src/main/java/com/example/shad2018_practical6/simpleexample/MainActivity.java
+++ SimpleExample/src/main/java/com/example/shad2018_practical6/simpleexample/MainActivity.java
@@ -2,55 +2,79 @@ package com.example.shad2018_practical6.simpleexample;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.MainThread;
+import android.support.annotation.WorkerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    static Thread sThread;
-    static Handler sHandler;
+    class ConcurrentJob {
+
+        Object mLock1 = new Object();
+        Object mLock2 = new Object();
+
+        /**
+         * https://developer.android.com/studio/write/annotations.html#thread-annotations
+         */
+        @WorkerThread
+        void doBackgroundJob() {
+            synchronized (mLock1) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+
+                synchronized (mLock2) {
+                    Log.d("Shad", "Background job finished");
+                    final TextView tv = mTextView;
+                    new Handler().post(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv.setText("Background job finished");
+                        }
+                    });
+                }
+            }
+        }
+
+        /**
+         * https://developer.android.com/studio/write/annotations.html#thread-annotations
+         */
+        @MainThread
+        void doMainThreadJob() {
+            synchronized (mLock2) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {}
+            }
+
+            synchronized (mLock1) {
+                Log.d("Shad", "Main thread job finished");
+                mTextView.setText("Main thread job finished");
+            }
+        }
+    }
+    private TextView mTextView;
+    private Thread mThread;
+    private ConcurrentJob mConcurrentJob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView tv = findViewById(R.id.main);
-        final long startTime = System.currentTimeMillis();
+        mTextView = findViewById(R.id.main);
+        mConcurrentJob = new ConcurrentJob();
 
-        if (sThread == null) {
-            sHandler = new Handler();
-            sThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    final long result = calculate();
-                    final long totalTime = System.currentTimeMillis() - startTime;
-                    Log.i("Shad", "Calculate finished");
-                    sHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Log.i("Shad", "Set value to tv = " + tv);
-                            tv.setText(String.format("Calculate value %d in %d milliseconds", result, totalTime));
-                        }
-                    });
-                }
-            });
-
-            sThread.start();
-        }
-    }
-
-    private int calculate() {
-        int result = 0;
-        for (int i = 1; i < 1000; i ++) {
-            result ++;
-            try {
-                Thread.sleep(5);
-            } catch (InterruptedException e) {
-
+        mThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mConcurrentJob.doBackgroundJob();
             }
-        }
-        return result;
+        });
+        mThread.start();
+
+        mConcurrentJob.doMainThreadJob();
     }
 }
