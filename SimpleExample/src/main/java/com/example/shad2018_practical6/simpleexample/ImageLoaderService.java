0fa3f509ea9a03d25205dd5d2d6e8fe213866457
Alexander Klintsov <alexklints@yandex-team.ru>
2018-02-04 01:31:19 +0300

    Load image via service

---
+++ SimpleExample/src/main/java/com/example/shad2018_practical6/simpleexample/ImageLoaderService.java
@@ -0,0 +1,76 @@
+package com.example.shad2018_practical6.simpleexample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.IBinder;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class ImageLoaderService extends Service {
+
+    private static final String TAG = "Shad";
+
+    public static final String SERVICE_ACTION_LOAD_IMAGE = "com.example.shad2018_practical6.simpleexample.LOAD_IMAGE";
+    public static final String BROADCAST_ACTION_UPDATE_IMAGE =
+            "com.example.shad2018_practical6.simpleexample.UPDATE_IMAGE";
+    public static final String BROADCAST_PARAM_IMAGE = "com.example.shad2018_practical6.simpleexample.IMAGE";
+
+
+    private final ImageLoader mImageLoader;
+
+    public ImageLoaderService() {
+        mImageLoader = new ImageLoader();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "ImageLoaderService#onCreate()");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "ImageLoaderService#onBind()");
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent,
+                              final int flags,
+                              final int startId) {
+        String action = intent.getAction();
+        Log.d(TAG, "ImageLoaderService#onStartCommand() with action = " + action);
+        if (SERVICE_ACTION_LOAD_IMAGE.equals(action)) {
+            load(startId);
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void load(final int startId) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final String imageUrl = mImageLoader.getImageUrl();
+                if (TextUtils.isEmpty(imageUrl) == false) {
+                    final Bitmap bitmap = mImageLoader.loadBitmap(imageUrl);
+                    final String imageName = "myImage.png";
+                    ImageSaver.getInstance().saveImage(getApplicationContext(), bitmap, imageName);
+
+                    final Intent intent = new Intent(BROADCAST_ACTION_UPDATE_IMAGE);
+                    intent.putExtra(BROADCAST_PARAM_IMAGE, imageName);
+                    sendBroadcast(intent);
+                }
+            }
+        }).start();
+
+        stopSelf(startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "MyService, onDestroy");
+        super.onDestroy();
+    }
+}
--- SimpleExample/src/main/java/com/example/shad2018_practical6/simpleexample/MainActivity.java
+++ SimpleExample/src/main/java/com/example/shad2018_practical6/simpleexample/MainActivity.java
@@ -1,45 +1,48 @@
 package com.example.shad2018_practical6.simpleexample;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private class ImageLoaderAsyncTask extends AsyncTask<Void, Void, Drawable> {
+    private class UpdateImageBroadcastReceiver extends BroadcastReceiver {
 
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mProgressBar.setVisibility(View.VISIBLE);
-        }
+        public void onReceive(final Context context, final Intent intent) {
+            String action = intent.getAction();
+            Log.d(TAG, "UpdateImageBroadcastReceiver#onReceive() with action");
+            if (ImageLoaderService.BROADCAST_ACTION_UPDATE_IMAGE.equals(action)) {
+                final String imageName = intent.getStringExtra(ImageLoaderService.BROADCAST_PARAM_IMAGE);
+                if (TextUtils.isEmpty(imageName) == false) {
+                    final Bitmap bitmap = ImageSaver.getInstance().loadImage(getApplicationContext(), imageName);
+                    final Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+                    setDrawable(drawable);
+                }
+            }
 
-        @Override
-        protected Drawable doInBackground(final Void... voids) {
-            return loadImage();
-        }
-
-        @Override
-        protected void onPostExecute(final Drawable drawable) {
-            super.onPostExecute(drawable);
-            setDrawable(drawable);
+            mProgressBar.setVisibility(View.INVISIBLE);
         }
     }
 
+    private static final String TAG = "Shad";
+
     private View mRootLayout;
     private View mProgressBar;
-    private ImageLoader mImageLoader;
 
-    private AsyncTask<Void, Void, Drawable> mDrawableLoaderAsyncTask;
+    private UpdateImageBroadcastReceiver mUpdateImageBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,31 +51,37 @@ public class MainActivity extends AppCompatActivity {
         final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mImageLoader = new ImageLoader();
         mRootLayout = findViewById(R.id.layout);
         mProgressBar = findViewById(R.id.progressBar);
-
-        mDrawableLoaderAsyncTask = new ImageLoaderAsyncTask();
 
         final FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                mDrawableLoaderAsyncTask.execute();
+                Log.d(TAG, "Load image");
+                mProgressBar.setVisibility(View.VISIBLE);
+                Intent intent = new Intent(MainActivity.this, ImageLoaderService.class)
+                        .setAction(ImageLoaderService.SERVICE_ACTION_LOAD_IMAGE);
+                startService(intent);
             }
         });
+
+        mUpdateImageBroadcastReceiver = new UpdateImageBroadcastReceiver();
     }
 
-    @Nullable
-    private Drawable loadImage() {
-        Drawable bitmapDrawable = null;
-        final String imageUrl = mImageLoader.getImageUrl();
-        if (TextUtils.isEmpty(imageUrl) == false) {
-            final Bitmap bitmap = mImageLoader.loadBitmap(imageUrl);
-            bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "MainActivity#onResume()");
+        registerReceiver(mUpdateImageBroadcastReceiver,
+                new IntentFilter(ImageLoaderService.BROADCAST_ACTION_UPDATE_IMAGE));
+    }
 
-        return bitmapDrawable;
+    @Override
+    protected void onPause() {
+        unregisterReceiver(mUpdateImageBroadcastReceiver);
+        Log.d(TAG, "MainActivity#onPause()");
+        super.onPause();
     }
 
     private void setDrawable(Drawable drawable) {
