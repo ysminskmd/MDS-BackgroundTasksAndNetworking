bc33af7bda559129a312cb4844eabfe30b6afb0f
Alexander Klintsov <alexklints@yandex-team.ru>
2018-02-06 03:29:41 +0300

    Use async task for calculation

--- SimpleExample/src/main/java/com/example/shad2018_practical6/simpleexample/MainActivity.java
+++ SimpleExample/src/main/java/com/example/shad2018_practical6/simpleexample/MainActivity.java
@@ -1,56 +1,103 @@
 package com.example.shad2018_practical6.simpleexample;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.annotation.MainThread;
+import android.support.annotation.WorkerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
-    static Thread sThread;
-    static Handler sHandler;
+    private static final String TAG = "Shad";
+
+    class CalculationAsyncTask extends AsyncTask<Integer, Integer, Integer> {
+
+        private int mResult;
+        private StringBuilder mStringBuilder = new StringBuilder();
+
+        @Override
+        @MainThread
+        protected void onPreExecute() {
+            Log.i(TAG, "CalculationAsyncTask#onPreExecute()");
+            mStringBuilder.append("Prepare calculation").append("\n");
+            mTv.setText(mStringBuilder.toString());
+        }
+
+        @Override
+        @WorkerThread
+        protected Integer doInBackground(Integer... startValue) {
+            Log.i(TAG, String.format("CalculationAsyncTask#doInBackground(%s)", Arrays.asList(startValue)));
+            mResult = startValue[0];
+            for (int i = 1; i < 1000; i ++) {
+                if (isCancelled() == false) {
+                    mResult ++;
+                    if (i % 100 == 0) {
+                        publishProgress(i/10);
+                    }
+                    try {
+                        Thread.sleep(5);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+            return mResult;
+        }
+
+        @MainThread
+        @Override
+        protected void onPostExecute(final Integer integer) {
+            super.onPostExecute(integer);
+            Log.i(TAG, "CalculationAsyncTask#onPostExecute");
+            mStringBuilder.append("Calculation finished with result = ").append(integer);
+            mTv.setText(mStringBuilder.toString());
+        }
+
+        @MainThread
+        @Override
+        protected void onProgressUpdate(final Integer... values) {
+            super.onProgressUpdate(values);
+            Log.i(TAG, String.format("CalculationAsyncTask#onProgressUpdate(%s)", Arrays.toString(values)));
+            int progress = values[0];
+            mStringBuilder.append("CurrentProgress: ").append(progress).append("%").append("\n");
+            mTv.setText(mStringBuilder.toString());
+        }
+
+        @MainThread
+        @Override
+        protected void onCancelled(final Integer integer) {
+            super.onCancelled(integer);
+            Log.i(TAG, "CalculationAsyncTask#onCanceled");
+            mStringBuilder.append("Canceled calculation with result: ").append(integer);
+            mTv.setText(mStringBuilder.toString());
+        }
+    }
+
+    private TextView mTv;
+    private CalculationAsyncTask mCalculationAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView tv = findViewById(R.id.main);
-        final long startTime = System.currentTimeMillis();
-
-        if (sThread == null) {
-            sHandler = new Handler();
-            sThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    final long result = calculate();
-                    final long totalTime = System.currentTimeMillis() - startTime;
-                    Log.i("Shad", "Calculate finished");
-                    sHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Log.i("Shad", "Set value to tv = " + tv);
-                            tv.setText(String.format("Calculate value %d in %d milliseconds", result, totalTime));
-                        }
-                    });
-                }
-            });
-
-            sThread.start();
-        }
+        mTv = findViewById(R.id.main);
+        mCalculationAsyncTask = new CalculationAsyncTask();
     }
 
-    private int calculate() {
-        int result = 0;
-        for (int i = 1; i < 1000; i ++) {
-            result ++;
-            try {
-                Thread.sleep(5);
-            } catch (InterruptedException e) {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mCalculationAsyncTask.execute(1000);
+    }
 
-            }
-        }
-        return result;
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mCalculationAsyncTask.cancel(true);
     }
 }
